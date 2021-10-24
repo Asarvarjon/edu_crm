3506d6ab660f9d1f3da850192dc17e2fd3daec8d
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-10-24 12:55:07 +0500

    fixed errors

--- src/controllers/UserRouteController.js
+++ src/controllers/UserRouteController.js
@@ -1,5 +1,10 @@
 module.exports = class UserController{
     static async SignInController(req, res, next) {
-        
+        try {
+            const {username, password} = req.body;
+
+        } catch (error) {
+            
+        }
     }
 }
--- src/routes/users/UserRoute.js
+++ src/routes/users/UserRoute.js
@@ -1,4 +1,7 @@
+const { SignInController } = require("../../controllers/UserRouteController");
+
 const UserRouter = require("express").Router();
 
+UserRouter.post("/sign_in", SignInController)
 
 module.exports = UserRouter
