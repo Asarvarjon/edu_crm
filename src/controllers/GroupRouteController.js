3283e63b934f8bdc64944166fb36921860c2e690
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-11-05 18:52:13 +0500

    Added group students route post, get, put, delete

--- src/controllers/GroupRouteController.js
+++ src/controllers/GroupRouteController.js
@@ -1,32 +1,34 @@
-const permissionChecker = require("../helpers/permissionChecker");  
-const { GroupCreateValidation } = require("../modules/validations");
+const permissionChecker = require("../helpers/permissionChecker");
+const {
+    GroupCreateValidation
+} = require("../modules/validations");
 
-module.exports = class GroupRouteController{
-    static async GroupCreatePostController(req, res,next) { 
-        try { 
-            permissionChecker("admin", req.user_permissions, res.error);  
+module.exports = class GroupRouteController {
+    static async GroupCreatePostController(req, res, next) {
+        try {
+            permissionChecker("admin", req.user_permissions, res.error);
             const data = await GroupCreateValidation(req.body, res.error);
-             
+
             const group = await req.db.groups.create({
                 group_time: data.time,
-                group_status:data.status,
+                group_status: data.status,
                 group_lesson_duration: data.lesson_duration,
                 group_course_duration: data.course_duration,
                 group_schedule: data.schedule,
                 course_id: data.course_id,
                 teacher_id: data.teacher_id
             })
- 
-              
+
+
             res.status(201).json({
-                ok:true,
+                ok: true,
                 message: "Group successfully created",
                 data: {
                     group
                 }
             })
 
-        } catch (error) { 
+        } catch (error) {
             next(error)
         }
     }
@@ -34,7 +36,7 @@ module.exports = class GroupRouteController{
 
     static async GroupUpdatecontroller(req, res, next) {
         try {
-            permissionChecker("admin", req.user_permissions, res.error);  
+            permissionChecker("admin", req.user_permissions, res.error);
             const data = await GroupCreateValidation(req.body, res.error);
 
             const group_id = req.params.group_id;
@@ -45,27 +47,25 @@ module.exports = class GroupRouteController{
                 ]
             });
 
-            if(!group_id) throw new error("Group not found");
-            
+            if (!group_id) throw new error("Group not found");
+
             await req.db.groups.update({
                 group_time: data.time,
-                group_status:data.status,
+                group_status: data.status,
                 group_lesson_duration: data.lesson_duration,
                 group_course_duration: data.course_duration,
                 group_schedule: data.schedule
-                
-            },
-             {
-                 where: { 
-                      group_id,
-                 }
-             }
-            )
+
+            }, {
+                where: {
+                    group_id,
+                }
+            })
 
             console.log(group);
-              
+
             res.status(201).json({
-                ok:true,
+                ok: true,
                 message: "Group successfully updated"
             })
 
@@ -78,26 +78,25 @@ module.exports = class GroupRouteController{
 
     static async GroupGetController(req, res, next) {
         try {
-            permissionChecker("admin", req.user_permissions, res.error);  
-            
+            permissionChecker("admin", req.user_permissions, res.error);
+
             const groups = await req.db.groups.findAll({
                 raw: true,
-                include: [
-                    {
-                        model: req.db.courses 
+                include: [{
+                        model: req.db.courses
                     },
                     {
-                        model: req.db.teachers 
+                        model: req.db.teachers
                     },
                     {
                         model: req.db.group_students
                     }
                 ]
-                    
+
             })
 
             res.json({
-                ok:true,
+                ok: true,
                 message: "Groups",
                 data: {
                     groups
@@ -107,13 +106,16 @@ module.exports = class GroupRouteController{
             next(error)
         }
     }
- 
-      
+
+
     static async AddApplicantToGroupController(req, res, next) {
         try {
-            permissionChecker("admin", req.user_permissions, res.error);  
+            permissionChecker("admin", req.user_permissions, res.error);
 
-            const {applicant_id, group_id} = req.body;
+            const {
+                applicant_id,
+                group_id
+            } = req.body;
 
             const new_student = await req.db.group_students.create({
                 group_student_id: applicant_id,
@@ -122,22 +124,135 @@ module.exports = class GroupRouteController{
 
             console.log(new_student);
 
-            await req.db.applicants.update({ 
-                applicant_status: "active" 
-            },
-             {
-                 where: { 
-                     applicant_id
-                 }
-             }
-            )
+            await req.db.applicants.update({
+                applicant_status: "active"
+            }, {
+                where: {
+                    applicant_id
+                }
+            })
 
             res.status(201).json({
                 ok: true,
                 message: "Applicant added to course succesfully"
             })
 
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    static async UpdateStudentPutController(req, res, next) {
+        try {
+            permissionChecker("admin", req.user_permissions, res.error);
+
+            const applicant_id = req.params.applicant_id;
+
+            const student = await req.db.group_students.findOne({
+                raw: true,
+                where: {
+                    group_student_id: applicant_id
+                }
+            });
+
+            if (!student) {
+                throw new res.error("Student not found")
+            };
+
+            const new_student = await req.db.group_students.update({
+                group_student_id: applicant_id,
+                group_id: group_id
+            }, {
+                where: {
+                    group_student_id: applicant_id
+                }
+            }); 
+
+            await req.db.applicants.update({
+                applicant_status: "active"
+            }, {
+                where: {
+                    applicant_id
+                }
+            })
+
+            res.status(200).json({
+                ok: true,
+                message: "Updated succesfully"
+            })
+
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    static async DeleteStudentFromGroupController(req, res, next) {
+        try {
+            permissionChecker("admin", req.user_permissions, res.error);
             
+            const student_id = req.params.student_id;
+
+            const student = await req.db.group_students.findOne({
+                raw: true,
+                where: {
+                    group_student_id: student_id
+                }
+            });
+
+            if (!student) {
+                throw new res.error("Student not found")
+            };
+
+            await req.db.group_students.destroy({
+                where: {
+                    group_student_id: student_id
+                }
+            });
+
+            await req.db.applicants.update({
+                applicant_status: "cancelled"
+            }, {
+                where: {
+                    applicant_id: student_id
+                }
+            });
+
+            res.status(200).json({
+                ok: true,
+                message: "Deleted succesfully"
+            })
+
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    static async GroupStudentsGetController(req, res, next){
+        try {
+            permissionChecker("admin", req.user_permissions, res.error);
+
+            const group_id = req.params.group_id;
+
+            const students = await req.db.group_students.findAll({
+                raw: true,
+                where:{
+                    group_id: group_id
+                }
+            })
+
+            res.status(200).json({
+                ok: true,
+                message: "GroupStudents",
+                data: {
+                    students
+                }
+            })
         } catch (error) {
             next(error)
         }
--- src/routes/groups/GroupRoute.js
+++ src/routes/groups/GroupRoute.js
@@ -1,4 +1,4 @@
-const { GroupCreatePostController, GroupUpdatecontroller, GroupGetController, AddApplicantToGroupController } = require("../../controllers/GroupRouteController");
+const { GroupCreatePostController, GroupUpdatecontroller, GroupGetController, AddApplicantToGroupController, UpdateStudentPutController, DeleteStudentFromGroupController, GroupStudentsGetController } = require("../../controllers/GroupRouteController");
 const authMiddleware = require("../../middlewares/authMiddleware");
 const permissionMiddleware = require("../../middlewares/permissionMiddleware");
 
@@ -8,7 +8,14 @@ GroupRouter.use([authMiddleware, permissionMiddleware]);
 GroupRouter.get("/", GroupGetController);
 GroupRouter.post("/", GroupCreatePostController);
 GroupRouter.put("/:group_id", GroupUpdatecontroller);
-GroupRouter.post("/student", AddApplicantToGroupController)
+GroupRouter.post("/student", AddApplicantToGroupController);  
+
+//
+GroupRouter.put("/student/:applicant_id", UpdateStudentPutController);
+GroupRouter.delete("/student/:student_id", DeleteStudentFromGroupController);
+GroupRouter.get("/:group_id", GroupStudentsGetController)
+
+
 
 
 
